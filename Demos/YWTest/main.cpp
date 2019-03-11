355d9b4d91c650ad323ff551a9b2b453bf504865
wangyanqing <wangyanqing@kuaishou.com>
2019-03-11 17:45:09 +0800

    add vulkan for mac os

--- Demos/YWTest/main.cpp
+++ Demos/YWTest/main.cpp
@@ -6,6 +6,10 @@
 //  Copyright © 2018年 FloyStudio. All rights reserved.
 //
 
+#define GLFW_INCLUDE_VULKAN
+
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 #include "YWPhysics/YWParticle.h"
 
@@ -13,7 +17,26 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     YPhy::Particle partice;
     
-    std::cout << "Hello, World!\n";
+    
+    glfwInit();
+    
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+    
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    
+    std::cout << extensionCount << " extensions supported" << std::endl;
+    
+    
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+    
+    glfwDestroyWindow(window);
+    
+    glfwTerminate();
     
     return 0;
 }
--- thirdsdk/README.md
+++ thirdsdk/README.md
@@ -1,4 +1,6 @@
 # third SDK
 glfw 3.2.1  #20190311
+vulkan sdk macos 1.1.101.0 #20180311
+vulkan sdk windows #to add
 
 
