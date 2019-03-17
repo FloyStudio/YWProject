627af90397cab6d11603a970d24d6ce43f9771f0
York Wang <wangchanggu@gmail.com>
2019-03-17 17:10:29 +0800

    add vulkan win sdk

--- Demos/YWTest/main.cpp
+++ Demos/YWTest/main.cpp
@@ -13,30 +13,104 @@
 #include <iostream>
 #include "YWPhysics/YWParticle.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    YPhy::Particle partice;
-    
-    
-    glfwInit();
-    
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-    
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-    
-    std::cout << extensionCount << " extensions supported" << std::endl;
-    
-    
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
-    
-    glfwDestroyWindow(window);
-    
-    glfwTerminate();
-    
-    return 0;
+#include <iostream>
+#include <stdexcept>
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+
+class HelloTriangleApplication
+{
+public:
+	void run()
+	{
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+private:
+	GLFWwindow* window;
+
+	VkInstance instance;
+
+	void initWindow()
+	{
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
+	void initVulkan()
+	{
+		createInstance();
+	}
+
+	void mainLoop()
+	{
+		while (!glfwWindowShouldClose(window))
+		{
+			glfwPollEvents();
+		}
+	}
+
+	void cleanup()
+	{
+		vkDestroyInstance(instance, nullptr);
+
+		glfwDestroyWindow(window);
+
+		glfwTerminate();
+	}
+
+	void createInstance()
+	{
+		VkApplicationInfo appInfo = {};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		unsigned int glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+		createInfo.enabledLayerCount = 0;
+
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create instance!");
+		}
+
+	}
+};
+
+int main()
+{
+	HelloTriangleApplication app;
+
+	try
+	{
+		app.run();
+	}
+	catch (const std::runtime_error& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
--- demos/YWHelloWorld/main.cpp
+++ demos/YWHelloWorld/main.cpp
@@ -57,13 +57,13 @@ int main(int argc, const char * argv[]) {
         glfwPollEvents();
     }
     
-    /*
+    
     const GLubyte* name = glGetString(GL_VENDOR);//返回负责当前OpenGL实现厂商的名字
     const GLubyte* biaoshifu = glGetString(GL_RENDERER);//返回一个渲染器标识符，通常是个硬件平台
     const GLubyte* OpenGLVersion = glGetString(GL_VERSION);//返回当前OpenGL实现的版本号
     std::cout<<name<<std::endl;
     std::cout<<biaoshifu<<std::endl;
-    std::cout<<OpenGLVersion<<std::endl;*/
+    std::cout<<OpenGLVersion<<std::endl;
     
     
     glfwTerminate();
